fe84e869b12c5eb48e53cd7684d63ce7d99829f9
MarcM <marina.ved80gmail.com>
2024-08-02 01:14:16 -0600

    add today weather info

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const userCityInput = document.querySelector('#search');
 const searchBtn = document.querySelector('#searchButton');
 
 //out data
+const todayWeatherOutput = document.querySelector('#weather-one');
 const cityHistoryOutput = document.querySelector('#city-history');
 
 //other data
+const APIKey = "f90bd61111a2ccd2c7a31a9e922d2b77";
 var citiesList = JSON.parse(localStorage.getItem("city")) || [];
 var addNewCity = {};
 
@@ -16,6 +18,43 @@ searchBtn.addEventListener('click', function (event) {
 
     //work with a Local Storage
     if (userCityInput.value) {
+
+        //get weather fot today
+
+        let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userCityInput.value}&appid=${APIKey}`;
+
+        fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+             //source: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_removechild_while
+            while (todayWeatherOutput.hasChildNodes()) {
+                todayWeatherOutput.removeChild(todayWeatherOutput.firstChild);
+            };
+
+            weatherTodayCard = document.createElement('div');
+
+            weatherTodayCity = document.createElement('p');
+
+            weatherTodayTemp = document.createElement('p');
+
+            weatherTodayWind = document.createElement('p');
+
+            weatherTodayHumdt = document.createElement('p');
+
+
+            weatherTodayCity.textContent = `${data.name}, ${data.sys.country}`;
+            weatherTodayTemp.textContent = `Temp: ${data.main.temp} F`;
+            weatherTodayWind.textContent = `Wind: ${data.wind.speed} MPH`;
+            weatherTodayHumdt.textContent = `Humidity: ${data.main.humidity} %`;
+        
+            weatherTodayCard.append(weatherTodayCity, weatherTodayTemp, weatherTodayWind, weatherTodayHumdt);
+            todayWeatherOutput.appendChild(weatherTodayCard);
+
+        }); 
 
         // save data to Local Storage
         addNewCity =  {
@@ -36,7 +75,7 @@ searchBtn.addEventListener('click', function (event) {
         //source: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_removechild_while
         while (cityHistoryOutput.hasChildNodes()) {
             cityHistoryOutput.removeChild(cityHistoryOutput.firstChild);
-          }
+          };
 
         for (let i = 0; i < getCitiesList.length; i++) {
             cityCard = document.createElement('div');
